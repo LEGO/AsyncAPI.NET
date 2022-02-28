6a529cc3ea9b2bf96284d57abe2220d199ee6262
AlexD <alex.dubrovskyi@consultant.lego.com>
2022-02-28 11:42:53 +0100

    fixing endlines to align across windows and unix OSs

--- test/LEGO.AsyncAPI.E2E.Tests/readers/samples/AsyncApi/ShouldConsumeProduceBase.cs
+++ test/LEGO.AsyncAPI.E2E.Tests/readers/samples/AsyncApi/ShouldConsumeProduceBase.cs
@@ -37,7 +37,7 @@ public class ShouldConsumeProduceBase<T>
     {
         var body = GetString(filename);
         var extensionsBody = ReadFile("LEGO/AsyncAPI/E2E/Tests/readers/samples/AsyncApi", "MockExtensions.json");
-        return new StringBuilder(body.Substring(0, body.Length - 2)).AppendLine(",").AppendLine(extensionsBody).Append("}").ToString();
+        return new StringBuilder(body.Substring(0, body.Length - 2).TrimEnd('\r')).AppendLine(",").AppendLine(extensionsBody).Append("}").ToString();
     }
 
     private string ReadFile(string sampleFolderPath, string filename)
