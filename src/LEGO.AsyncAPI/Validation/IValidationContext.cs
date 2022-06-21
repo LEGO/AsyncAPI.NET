3286527dbd7d78ebfa7456df76c3a59de5a7dc8b
Michael Friis <michael.nissen.thorup.friis@lego.com>
2022-06-21 11:15:54 +0200

    some rules for thy but not for i

---
+++ src/LEGO.AsyncAPI/Validation/IValidationContext.cs
@@ -0,0 +1,39 @@
+using LEGO.AsyncApi.Validations;
+
+namespace LEGO.AsyncApi.Validations
+{
+    /// <summary>
+    /// Constrained interface used to provide context to rule implementation
+    /// </summary>
+    public interface IValidationContext
+    {
+        /// <summary>
+        /// Register an error with the validation context.
+        /// </summary>
+        /// <param name="error">Error to register.</param>
+        void AddError(AsyncApiValidatorError error);
+
+        /// <summary>
+        /// Register a warning with the validation context.
+        /// </summary>
+        /// <param name="warning">Warning to register.</param>
+        void AddWarning(AsyncApiValidatorWarning warning);
+
+        /// <summary>
+        /// Allow Rule to indicate validation error occured at a deeper context level.  
+        /// </summary>
+        /// <param name="segment">Identifier for context</param>
+        void Enter(string segment);
+
+        /// <summary>
+        /// Exit from path context elevel.  Enter and Exit calls should be matched.
+        /// </summary>
+        void Exit();
+
+        /// <summary>
+        /// Pointer to source of validation error in document
+        /// </summary>
+        string PathString { get; }
+
+    }
+}
