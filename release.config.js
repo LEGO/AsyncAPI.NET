f28b137eb695c9dd15da75253ceaded600921c27
Alex Wichmann <VisualBean@users.noreply.github.com>
2022-11-23 08:24:44 +0100

    ci: update release to not include changelog

--- release.config.js
+++ release.config.js
@@ -3,17 +3,17 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        "changelogFile": "CHANGELOG.md"
-      }
-    ],
-    [
-      "@semantic-release/git",
-      {
-        "assets": ["CHANGELOG.md"]
-      }
-    ]
-  ]
+//     [
+//       "@semantic-release/changelog",
+//       {
+//         "changelogFile": "CHANGELOG.md"
+//       }
+//     ],
+//     [
+//       "@semantic-release/git",
+//       {
+//         "assets": ["CHANGELOG.md"]
+//       }
+//     ]
+   ]
 }
