f17a4ff32bd358ee88b424925ee441e4b6f0bc11
Alex Wichmann <VisualBean@users.noreply.github.com>
2022-11-23 09:03:19 +0100

    fix: presets not needed. Angular convention is fine

--- release.config.js
+++ release.config.js
@@ -1,18 +1,8 @@
 module.exports = {
   branches: "main",
   plugins: [
-    [
       "@semantic-release/commit-analyzer",
-      {
-        "preset": "conventionalcommits" 
-      }
-    ],
-    [
       "@semantic-release/release-notes-generator", 
-     {
-       "preset": "conventionalcommits"
-     }
-    ],
      [
        "@semantic-release/changelog",
        {
