8adb01a663160b944f2e8bf732019de8eb160a10
Alex Wichmann <VisualBean@users.noreply.github.com>
2022-11-23 08:46:00 +0100

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   branches: "main",
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "conventional-changelog" 
+      }
+    ],
+    [
+      "@semantic-release/release-notes-generator", 
+     {
+       "preset": "conventional-changelog"
+     }
+    ],
      [
        "@semantic-release/changelog",
        {
