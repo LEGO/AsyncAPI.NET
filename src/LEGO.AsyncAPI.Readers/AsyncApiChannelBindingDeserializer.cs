3b1984bba7190b47e3ddafdd7fcaf866ba75d3b4
Alex Wichmann <alexander.wichmann.carlsen@lego.com>
2022-11-24 11:36:55 +0100

    Add websockets binding

--- src/LEGO.AsyncAPI.Readers/AsyncApiChannelBindingDeserializer.cs
+++ src/LEGO.AsyncAPI.Readers/AsyncApiChannelBindingDeserializer.cs
@@ -44,6 +44,8 @@ namespace LEGO.AsyncAPI.Readers
                     return LoadBinding("ChannelBinding", property.Value, kafkaChannelBindingFixedFields);
                 case BindingType.Pulsar:
                     return LoadBinding("ChannelBinding", property.Value, pulsarChannelBindingFixedFields);
+                case BindingType.Websockets:
+                    return LoadBinding("ChannelBinding", property.Value, webSocketsChannelBindingFixedFields);
                 default:
                     throw new System.Exception("ChannelBinding not found");
             }
