e1a3302aa1b2b790622d15ca7565c2c089a04137
Dec Kolakowski <dec.kolakowski@justeattakeaway.com>
2023-06-05 11:01:31 +0100

    feat: fix merge errors

--- src/LEGO.AsyncAPI.Readers/V2/AsyncApiDeserializer.cs
+++ src/LEGO.AsyncAPI.Readers/V2/AsyncApiDeserializer.cs
@@ -19,38 +19,6 @@ namespace LEGO.AsyncAPI.Readers
     using LEGO.AsyncAPI.Models.Interfaces;
     using LEGO.AsyncAPI.Readers.ParseNodes;
 
-    public static class ExtensionHelpers
-    {
-        internal static PatternFieldMap<T> GetExtensionsFieldMap<T>() where T : IAsyncApiExtensible
-        {
-            return new ()
-            {
-                {
-                    s => s.StartsWith("x-"),
-                    (a, p, n) => a.AddExtension(p, LoadExtension(p, n))
-                }, 
-            };
-        }
-
-        public static IAsyncApiExtension LoadExtension(string name, ParseNode node)
-        {
-            try
-            {
-                if (node.Context.ExtensionParsers.TryGetValue(name, out var parser))
-                {
-                    return parser(
-                        AsyncApiAnyConverter.GetSpecificAsyncApiAny(node.CreateAny()));
-                }
-            }
-            catch (AsyncApiException ex)
-            {
-                ex.Pointer = node.Context.GetLocation();
-                node.Context.Diagnostic.Errors.Add(new AsyncApiError(ex));
-            }
-
-            return AsyncApiAnyConverter.GetSpecificAsyncApiAny(node.CreateAny());
-        }
-    }
     internal static partial class AsyncApiV2Deserializer
     {
         internal static void ParseMap<T>(
