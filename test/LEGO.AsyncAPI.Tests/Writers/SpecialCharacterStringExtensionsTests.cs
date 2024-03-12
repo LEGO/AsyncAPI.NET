d11fdfaf7525a9d501108f9c57395ef8e40e6de4
Byron Mayne <Byronmayne@gmail.com>
2024-03-11 20:48:12 -0400

    Added more tests for date time formats

--- test/LEGO.AsyncAPI.Tests/Writers/SpecialCharacterStringExtensionsTests.cs
+++ test/LEGO.AsyncAPI.Tests/Writers/SpecialCharacterStringExtensionsTests.cs
@@ -63,14 +63,26 @@ namespace LEGO.AsyncAPI.Tests.Writers
         [Test]
         public void GetYamlCompatibleString_DateTimeSlashString_WrappedWithQuotes()
             => this.Compose("12/31/2022 23:59:59", "'12/31/2022 23:59:59'");
-           
+
         [Test]
         public void GetYamlCompatibleString_DateTimeDashString_WrappedWithQuotes()
             => this.Compose("2022-12-31 23:59:59", "'2022-12-31 23:59:59'");
-            
+
         [Test]
         public void GetYamlCompatibleString_DateTimeISOString_NotWrappedWithQuotes()
             => this.Compose("2022-12-31T23:59:59Z", "2022-12-31T23:59:59Z");
+
+        [Test]
+        public void GetYamlCompatibleString_DateTimeCanonicalString_NotWrappedWithQuotes()
+            => this.Compose("2001-12-15T02:59:43.1Z", "2001-12-15T02:59:43.1Z");
+
+        [Test]
+        public void GetYamlCompatibleString_DateTimeSpacedString_NotWrappedWithQuotes()
+            => this.Compose("2001-12-14 21:59:43.10 -5", "2001-12-14 21:59:43.10 -5");
+
+        [Test]
+        public void GetYamlCompatibleString_DateString_NotWrappedWithQuotes()
+            => this.Compose("2002-12-14", "2002-12-14");
 
         [Test]
         [TestCase("\0", "\\0")]
