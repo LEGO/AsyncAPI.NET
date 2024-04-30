697f45bb2abb1fd79bfd918889d9e00f7b9f9fdf
dec.kolakowski <dec.kolakowski@justeattakeaway.com>
2024-04-30 14:45:28 +0100

    feat: add doc strings for interface

--- src/LEGO.AsyncAPI.Readers/IAsyncApiExternalReferenceReader.cs
+++ src/LEGO.AsyncAPI.Readers/IAsyncApiExternalReferenceReader.cs
@@ -1,6 +1,14 @@
 namespace LEGO.AsyncAPI.Readers;
 
+/// <summary>
+/// Interface that provides method for reading external references.å
+/// </summary>
 public interface IAsyncApiExternalReferenceReader
 {
+    /// <summary>
+    /// Method that returns the AsyncAPI content that the external reference from the $ref points to.
+    /// </summary>
+    /// <param name="reference">The content address of the $ref.</param>
+    /// <returns>The content of the reference as a string.</returns>
     public string GetExternalResource(string reference);
 }
