6f8d6f7277f6194c6d25b2110d37a77d6760c669
Alex Wichmann <VisualBean@users.noreply.github.com>
2025-01-23 13:16:26 +0100

    chore: add channelKey validation rule (#202)

--- src/LEGO.AsyncAPI/Validation/Rules/AsyncApiDocumentRules.cs
+++ src/LEGO.AsyncAPI/Validation/Rules/AsyncApiDocumentRules.cs
@@ -2,6 +2,8 @@
 
 namespace LEGO.AsyncAPI.Validation.Rules
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Text.RegularExpressions;
     using LEGO.AsyncAPI.Models;
@@ -10,10 +12,13 @@ namespace LEGO.AsyncAPI.Validation.Rules
     [AsyncApiRule]
     public static class AsyncApiDocumentRules
     {
+        private static TimeSpan RegexTimeout = TimeSpan.FromSeconds(1);
+
         /// <summary>
         /// The key regex.
         /// </summary>
-        public static Regex KeyRegex = new Regex(@"^[a-zA-Z0-9\.\-_]+$");
+        public static Regex KeyRegex = new Regex(@"^[a-zA-Z0-9\.\-_]+$", RegexOptions.None, RegexTimeout);
+        public static Regex ChannelKeyUriTemplateRegex = new Regex(@"^(?:(?:[^\x00-\x20""'<>%\\^`{|}]|%[0-9a-f]{2})|\{[+#./;?&=,!@|]?(?:[a-z0-9_]|%[0-9a-f]{2})+(?::[1-9][0-9]{0,3}|\*)?(?:,(?:[a-z0-9_]|%[0-9a-f]{2})+(?::[1-9][0-9]{0,3}|\*)?)*\})*$", RegexOptions.IgnoreCase, RegexTimeout);
 
         public static ValidationRule<AsyncApiDocument> DocumentRequiredFields =>
             new ValidationRule<AsyncApiDocument>(
@@ -30,15 +35,57 @@ namespace LEGO.AsyncAPI.Validation.Rules
                     context.Exit();
 
                     context.Enter("channels");
-                    if (document.Channels == null || !document.Channels.Keys.Any())
+                    try
                     {
-                        context.CreateError(
-                            nameof(DocumentRequiredFields),
-                            string.Format(Resource.Validation_FieldRequired, "channels", "document"));
-                    }
+                        // MUST have at least 1 channel
+                        if (document.Channels == null || !document.Channels.Keys.Any())
+                        {
+                            context.CreateError(
+                                nameof(DocumentRequiredFields),
+                                string.Format(Resource.Validation_FieldRequired, "channels", "document"));
+                            return;
+                        }
+                        var hashSet = new HashSet<string>();
+                        foreach (var key in document.Channels.Keys)
+                        {
+                            // Uri-template
+                            if (!ChannelKeyUriTemplateRegex.IsMatch(key))
+                            {
+                                context.CreateError(
+                                    "ChannelKeys",
+                                    string.Format(Resource.Validation_KeyMustMatchRegularExpr, key, "channels", KeyRegex.ToString()));
+                            }
 
-                    context.Exit();
+                            // Unique channel keys
+                            var pathSignature = GetKeySignature(key);
+                            if (!hashSet.Add(pathSignature))
+                            {
+                                context.CreateError("ChannelKey", string.Format(Resource.Validation_ChannelsMustBeUnique, pathSignature));
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        context.Exit();
+                    }                    
                 });
+
+        private static string GetKeySignature(string path)
+        {
+            for (int openBrace = path.IndexOf('{'); openBrace > -1; openBrace = path.IndexOf('{', openBrace + 2))
+            {
+                int closeBrace = path.IndexOf('}', openBrace);
+
+                if (closeBrace < 0)
+                {
+                    return path;
+                }
+
+                path = path.Substring(0, openBrace + 1) + path.Substring(closeBrace);
+            }
+
+            return path;
+        }
 
         public static ValidationRule<AsyncApiDocument> KeyMustBeRegularExpression =>
             new ValidationRule<AsyncApiDocument>(
