f271fc8688d3ee76838f0b44a8f72af2693a3c52
DominikKaloc <dominik.kaloc@lego.com>
2026-01-15 14:42:39 +0100

    chore: serialize null objects

--- src/LEGO.AsyncAPI/Writers/AsyncApiWriterExtensions.cs
+++ src/LEGO.AsyncAPI/Writers/AsyncApiWriterExtensions.cs
@@ -301,10 +301,7 @@ namespace LEGO.AsyncAPI.Writers
             Action<IAsyncApiWriter, string, T> action)
             where T : IAsyncApiElement
         {
-            if (elements != null && elements.Any())
-            {
-                writer.WriteMapInternal(name, elements, action);
-            }
+            writer.WriteMapInternal(name, elements, action);
         }
 
         /// <summary>
